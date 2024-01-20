a8de224e96797f2f64ec3b1172598224cff38862
Jeremiah Williams <jeremiah1066@users.noreply.github.com>
2024-01-19 17:13:59 -0800

    Update influx.py

--- influx.py
+++ influx.py
@@ -4,7 +4,7 @@ from influxdb_client import InfluxDBClient, Point, WritePrecision
 from influxdb_client.client.write_api import SYNCHRONOUS
 
 # You can generate a Token from the "Tokens Tab" in the UI
-token = "XO2XsIF_k5PLhnxvqzY997QLBjF1Ygx2xD2iH9BKqx88QsWis5FyQTqUqsFgEiiMw87qOVVaZmhJyB9Og3C8BQ=="
+token = ""
 org = "iracing"
 bucket = "iracing_data"
 
